d07b70cacca9ddb61c5d521a8cc327cf5d6fac79
Andrea Maiolo <andrea.maiolo@hotmail.it>
2019-04-22 20:33:16 +0200

    Generate different CSV files for cities, provinces, regions and zip codes

--- build.js
+++ build.js
@@ -1,21 +1,77 @@
-const fs   = require('fs');
+const fs = require('fs');
 const path = require('path');
+const slugify = require('@sindresorhus/slugify');
 
-let dataPath = path.join(__dirname, 'data');
-let files = fs.readdirSync(dataPath);
+const files = fs.readdirSync(path.join(__dirname, 'data'));
+const citiesCsvFile = path.join(__dirname, 'cities.csv');
+const provincesCsvFile = path.join(__dirname, 'provinces.csv');
+const regionsCsvFile = path.join(__dirname, 'regions.csv');
+const zipCodesCsvFile = path.join(__dirname, 'zip-codes.csv');
 
-let data = [];
+let cities = [];
+let provinces = [];
+let regions = [];
 
-files.forEach((f) => {
-	let filePath = path.join(__dirname, 'data', f);
-	let content = fs.readFileSync(filePath).toString();
-	let obj = JSON.parse(content);
-	data.push(obj);
+let citiesCsv = '';
+let provincesCsv = '';
+let regionsCsv = '';
+let zipCodesCsv = '';
+
+files.forEach(file => {
+	const data = JSON.parse(fs.readFileSync(path.join(__dirname, 'data', file)));
+
+	const cityCode = data.codice;
+	const provinceCode = data.provincia.codice;
+	const regionCode = data.regione.codice;
+	const cityName = data.nome;
+	const regionName = data.regione.nome;
+
+	cities.push({
+		provinceCode: provinceCode,
+		code: cityCode,
+		name: cityName,
+		slug: slugify(cityName.split('/')[0])
+	});
+	
+	if (provinces.length === 0 || !provinces.some(province => province.code === provinceCode)) {
+		provinces.push({
+			regionCode: regionCode,
+			code: provinceCode,
+			name: data.provincia.nome,
+			acronym: data.sigla
+		});
+	}
+
+	if (regions.length === 0 || !regions.some(region => region.code === regionCode)) {
+		regions.push({
+			code: regionCode,
+			name: regionName,
+			slug: slugify(regionName.split('/')[0])
+		});
+	}
+
+	for (const zipCode of data.cap) {
+		zipCodesCsv += `0,${cityCode},${zipCode}\n`;
+	}
 });
 
-data.sort((a, b) => {
-	return a['nome'].localeCompare(b['nome']);
+cities.sort((a, b) => a['name'].localeCompare(b['name']));
+provinces.sort((a, b) => a['name'].localeCompare(b['name']));
+regions.sort((a, b) => a['name'].localeCompare(b['name']));
+
+cities.forEach(city => {
+	citiesCsv += `0,${city.provinceCode},${city.code},${city.name},${city.slug}\n`;
 });
 
-let outFilePath = path.join(__dirname, 'comuni.json');
-fs.writeFileSync(outFilePath, JSON.stringify(data));
+provinces.forEach(province => {
+	provincesCsv += `0,${province.regionCode},${province.code},${province.name},${province.acronym}\n`;
+});
+
+regions.forEach(region => {
+	regionsCsv += `0,${region.code},${region.name},${region.slug}\n`;
+});
+
+fs.writeFileSync(citiesCsvFile, citiesCsv);
+fs.writeFileSync(provincesCsvFile, provincesCsv);
+fs.writeFileSync(regionsCsvFile, regionsCsv);
+fs.writeFileSync(zipCodesCsvFile, zipCodesCsv);
